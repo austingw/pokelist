1b74900263e824abf2125542255d42c2ce34d197
austingw <senortinman@gmail.com>
2023-10-01 14:53:14 -0400

    Implement basic modal logic and row styling

--- src/app/components/PokeList.tsx
+++ src/app/components/PokeList.tsx
@@ -1,28 +1,69 @@
-import { Image, Group, ScrollArea, Text } from "@mantine/core";
+"use client";
+
+import { Image, Group, ScrollArea, Text, Stack, Modal } from "@mantine/core";
 import { Pokemon } from "../types/Pokemon";
+import classes from "./PokeList.module.css";
+import Details from "./Details";
+import { useDisclosure } from "@mantine/hooks";
+import { useState } from "react";
 
 interface PokeListProps {
   initialData: Pokemon[];
 }
 
 const PokeList = ({ initialData }: PokeListProps) => {
-  return (
-    <ScrollArea>
-      {initialData.map((pokemon: any) => {
-        return (
-          <Group align="center" key={pokemon.name}>
-            <Image
-              src={pokemon.sprites.front_default}
-              alt={pokemon.name}
-              w={100}
-              h={100}
-            />
+  const [selectedPokemon, setSelectedPokemon] = useState<Pokemon | null>(null);
+  const [opened, { open, close }] = useDisclosure(false);
 
-            <Text>{pokemon.name}</Text>
-          </Group>
-        );
-      })}
-    </ScrollArea>
+  const handleClick = (pokemon: Pokemon) => {
+    setSelectedPokemon(pokemon);
+  };
+
+  const handleClose = () => {
+    setSelectedPokemon(null);
+  };
+
+  return (
+    <>
+      <ScrollArea>
+        <Stack w={"100%"} h={"100%"}>
+          {initialData.map((pokemon: any) => {
+            return (
+              <Group
+                align="center"
+                justify="center"
+                w={"100%"}
+                key={pokemon.name}
+                className={classes.row}
+                onClick={() => {
+                  handleClick(pokemon);
+                  open();
+                }}
+              >
+                <Text>{pokemon.id}</Text>
+                <Image
+                  src={pokemon.sprites.front_default}
+                  alt={pokemon.name}
+                  w={100}
+                  h={100}
+                />
+
+                <Text>{pokemon.name}</Text>
+              </Group>
+            );
+          })}
+        </Stack>
+      </ScrollArea>
+      <Modal
+        opened={opened}
+        onClose={close}
+        size="calc(100vw - 3rem)"
+        withCloseButton={false}
+        padding={0}
+      >
+        <Details pokemon={selectedPokemon} />
+      </Modal>
+    </>
   );
 };
 
