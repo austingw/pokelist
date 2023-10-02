4a0c4b0a4ede879afba9fc0f9b5928015e47a624
austingw <senortinman@gmail.com>
2023-10-02 07:52:21 -0400

    Add search input, pagination, and loading overlay

--- src/app/components/PokeList.tsx
+++ src/app/components/PokeList.tsx
@@ -1,19 +1,47 @@
 "use client";
 
-import { Image, Group, ScrollArea, Text, Stack, Modal } from "@mantine/core";
+import {
+  Image,
+  Group,
+  ScrollArea,
+  Text,
+  Stack,
+  Modal,
+  TextInput,
+  LoadingOverlay,
+  Pagination,
+} from "@mantine/core";
 import { Pokemon } from "../types/Pokemon";
 import classes from "./PokeList.module.css";
 import Details from "./Details";
-import { useDisclosure } from "@mantine/hooks";
+import { useDebouncedValue, useDisclosure } from "@mantine/hooks";
 import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import getList from "../utils/getList";
 
 interface PokeListProps {
   initialData: Pokemon[];
 }
 
 const PokeList = ({ initialData }: PokeListProps) => {
+  const [value, setValue] = useState("");
+  const [activePage, setPage] = useState(1);
   const [selectedPokemon, setSelectedPokemon] = useState<Pokemon | null>(null);
   const [opened, { open, close }] = useDisclosure(false);
+  const [debounced] = useDebouncedValue(value, 100);
+
+  //Using Tanstack Query to fetch search results/pages
+  const { data, isFetching } = useQuery({
+    queryKey: ["list", debounced, activePage],
+    queryFn: () =>
+      getList({
+        searchTerm: debounced,
+        page: activePage,
+      }),
+    initialData,
+    refetchOnWindowFocus: false,
+    keepPreviousData: true,
+  });
 
   const handleClick = (pokemon: Pokemon) => {
     setSelectedPokemon(pokemon);
@@ -21,13 +49,22 @@ const PokeList = ({ initialData }: PokeListProps) => {
 
   const handleClose = () => {
     setSelectedPokemon(null);
+    close();
   };
 
   return (
     <>
       <ScrollArea>
         <Stack w={"100vw"} h={"100%"} py={10} align="center">
-          {initialData.map((pokemon: any) => {
+          <LoadingOverlay visible={isFetching} />
+          <TextInput
+            value={value}
+            onChange={(event) => setValue(event.currentTarget.value)}
+            size="xl"
+            w={"80%"}
+            placeholder="Search by name or Pokédex #"
+          />
+          {data?.map((pokemon: any) => {
             return (
               <Group
                 align="center"
@@ -39,6 +76,7 @@ const PokeList = ({ initialData }: PokeListProps) => {
                   handleClick(pokemon);
                   open();
                 }}
+                px={20}
               >
                 <Text
                   fz={30}
@@ -58,11 +96,24 @@ const PokeList = ({ initialData }: PokeListProps) => {
               </Group>
             );
           })}
+          {
+            //conditionally render pagination to only show up if there's no search term
+          }
+          {debounced === "" && (
+            <Pagination
+              value={activePage}
+              onChange={setPage}
+              total={8}
+              radius="xl"
+              withEdges
+              color="black"
+            />
+          )}
         </Stack>
       </ScrollArea>
       <Modal
         opened={opened}
-        onClose={close}
+        onClose={handleClose}
         size="75vw"
         centered
         withCloseButton={false}
