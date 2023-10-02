cbb1e1f9d6ddd17290560c719a96a38843699ca8
austingw <senortinman@gmail.com>
2023-10-02 07:51:17 -0400

    Update getList for for search/pagination

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,38 +4,40 @@ import getList from "./utils/getList";
 import { Flex, Group, Text } from "@mantine/core";
 
 export default async function Home() {
-  const initialData: Pokemon[] = await getList();
+  //get intial data for SSR
+  const initialData: Pokemon[] = await getList({
+    searchTerm: "me",
+    page: 1,
+  });
 
   return (
-    <>
-      <Flex
-        h={"100%"}
+    <Flex
+      h={"100%"}
+      w={"100%"}
+      direction={"column"}
+      align={"center"}
+      justify={"flex-start"}
+    >
+      <Group
+        h={200}
         w={"100%"}
-        direction={"column"}
-        align={"center"}
-        justify={"center"}
+        align="center"
+        justify="center"
+        px={30}
+        bg={"black"}
+        style={{
+          boxShadow: "0px 0px 10px 0px rgba(0,0,0,0.75)",
+        }}
       >
-        <Group
-          h={200}
-          w={"100%"}
-          align="center"
-          justify="center"
-          px={30}
-          bg={"black"}
-          style={{
-            boxShadow: "0px 0px 10px 0px rgba(0,0,0,0.75)",
-          }}
+        <Text
+          fz={80}
+          variant="gradient"
+          gradient={{ from: "#ef3b36", to: "white" }}
         >
-          <Text
-            fz={80}
-            variant="gradient"
-            gradient={{ from: "#ef3b36", to: "white" }}
-          >
-            Pokélist
-          </Text>
-        </Group>
-        <PokeList initialData={initialData} />
-      </Flex>
-    </>
+          Pokélist
+        </Text>
+      </Group>
+      <PokeList initialData={initialData} />
+    </Flex>
   );
 }
--- src/app/utils/getList.ts
+++ src/app/utils/getList.ts
@@ -1,8 +1,18 @@
 import axios from "axios";
 
-const getList = async () => {
+interface getListParams {
+  searchTerm: string;
+  page: number;
+}
+
+const getList = async ({ searchTerm, page }: getListParams) => {
   const response = await axios
-    .get(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/pokemon`)
+    .get(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/pokemon`, {
+      params: {
+        searchTerm,
+        page,
+      },
+    })
     .catch((err) => {
       console.error("Err", err);
     })
